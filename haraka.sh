b2138a5466f6587d0df62782a191f07601390e43
hdh <hdh@cvoit.com>
2025-06-03 18:24:13 +0800

    修改项目启动文件的启动方式为 node /usr/local/haraka/bin/haraka xxx

--- haraka.sh
+++ haraka.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec haraka -c /usr/local/haraka --verbose 2>&1
+#exec haraka -c /usr/local/haraka --verbose 2>&1
+exec node /usr/local/haraka/bin/haraka -c /usr/local/haraka --verbose 2>&1
